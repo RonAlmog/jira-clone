06129395705a035178eb11e297020168cc622d64
Ron Almog <ron8132@gmail.com>
2024-11-09 16:45:47 -0500

    customize height for select

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
-    <div className="flex gap-4">
+    <div className="flex flex-col gap-4 w-40 p-2">
       <Button variant="primary" size="lg">
         Primary
       </Button>
@@ -15,6 +16,7 @@ export default function Home() {
       <Button variant="outline">outline</Button>
       <Button variant="muted">Muted</Button>
       <Button variant="teritary">teritary</Button>
+      <Input />
     </div>
   );
 }
