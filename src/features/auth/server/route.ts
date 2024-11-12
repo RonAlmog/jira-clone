b9dbdd71de0c056e69332e4b4bb1e954b08a223f
Ron Almog <ron8132@gmail.com>
2024-11-11 20:16:45 -0500

    complete registration with appwrite api

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -2,6 +2,10 @@ import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
 import { loginSchema, registerSchema } from "../schemas";
 import { z } from "zod";
+import { createAdminClient } from "@/lib/appwrite";
+import { ID } from "node-appwrite";
+import { setCookie } from "hono/cookie";
+import { AUTH_COOKIE } from "../constants";
 
 const app = new Hono()
   .post(
@@ -20,7 +24,18 @@ const app = new Hono()
   .post("/register", zValidator("json", registerSchema), async (c) => {
     const { name, email, password } = c.req.valid("json");
     console.log({ name, email, password });
-    return c.json({ name, email, password });
+    const { account } = await createAdminClient();
+    const user = await account.create(ID.unique(), email, password, name);
+
+    const session = await account.createEmailPasswordSession(email, password);
+    setCookie(c, AUTH_COOKIE, session.secret, {
+      path: "/",
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 60 * 60 + 24 * 30,
+    });
+    return c.json({ data: user });
   });
 
 export default app;
