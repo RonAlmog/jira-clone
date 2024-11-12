50f659e8928f3bdcc99774ec43c7dcdc9ccb6765
Ron Almog <ron8132@gmail.com>
2024-11-12 07:34:48 -0500

    add login, logout backend code

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -4,7 +4,7 @@ import { loginSchema, registerSchema } from "../schemas";
 import { z } from "zod";
 import { createAdminClient } from "@/lib/appwrite";
 import { ID } from "node-appwrite";
-import { setCookie } from "hono/cookie";
+import { deleteCookie, setCookie } from "hono/cookie";
 import { AUTH_COOKIE } from "../constants";
 
 const app = new Hono()
@@ -16,16 +16,25 @@ const app = new Hono()
     async (c) => {
       const { email, password } = c.req.valid("json");
       // const {userId}=c.req.valid('param');
+      const { account } = await createAdminClient();
+      const session = await account.createEmailPasswordSession(email, password);
+      setCookie(c, AUTH_COOKIE, session.secret, {
+        path: "/",
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+        maxAge: 60 * 60 + 24 * 30,
+      });
 
-      console.log({ email, password });
-      return c.json({ email, password });
+      // console.log({ email, password });
+      return c.json({ success: true });
     }
   )
   .post("/register", zValidator("json", registerSchema), async (c) => {
     const { name, email, password } = c.req.valid("json");
-    console.log({ name, email, password });
+    // console.log({ name, email, password });
     const { account } = await createAdminClient();
-    const user = await account.create(ID.unique(), email, password, name);
+    await account.create(ID.unique(), email, password, name);
 
     const session = await account.createEmailPasswordSession(email, password);
     setCookie(c, AUTH_COOKIE, session.secret, {
@@ -35,7 +44,11 @@ const app = new Hono()
       sameSite: "strict",
       maxAge: 60 * 60 + 24 * 30,
     });
-    return c.json({ data: user });
+    return c.json({ success: true });
+  })
+  .post("/logout", (c) => {
+    deleteCookie(c, AUTH_COOKIE);
+    return c.json({ success: true });
   });
 
 export default app;
