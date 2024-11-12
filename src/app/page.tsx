ec904f1e41cbf79a4dbc9a5b04fa91dcec8bd16b
Ron Almog <ron8132@gmail.com>
2024-11-12 08:41:38 -0500

    use logout to redirect, invalidate hook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  const { data, isLoading } = useCurrent();
+  const { mutate } = useLogout();
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+    }
+  }, [data]);
   return (
     <div className="flex flex-col gap-4 w-40 p-2">
-      <Button variant="primary" size="lg">
+      only logged in users!
+      <Button onClick={() => mutate()}>Log out</Button>
+      {/* <Button variant="primary" size="lg">
         Primary
       </Button>
       <Button variant="secondary" size="sm">
@@ -16,7 +32,7 @@ export default function Home() {
       <Button variant="outline">outline</Button>
       <Button variant="muted">Muted</Button>
       <Button variant="teritary">teritary</Button>
-      <Input />
+      <Input /> */}
     </div>
   );
 }
