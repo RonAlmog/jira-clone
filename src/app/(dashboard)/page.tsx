411a8ac4602ce836eb67d5d87a6ef50622730b1d
Ron Almog <ron8132@gmail.com>
2024-11-16 16:45:10 -0500

    redirect to last workspace

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -4,6 +4,7 @@ import { getCurrent } from "@/features/auth/actions";
 import { useCurrent } from "@/features/auth/api/use-current";
 import { useLogout } from "@/features/auth/api/use-logout";
 import { UserButton } from "@/features/auth/components/user-button";
+import { getWorkspaces } from "@/features/workspaces/actions";
 import { CreateWorkspaceForm } from "@/features/workspaces/components/create-workspace-form";
 import { redirect, useRouter } from "next/navigation";
 import { useEffect } from "react";
@@ -13,9 +14,17 @@ export default async function Home() {
   if (!user) {
     redirect("/sign-in");
   }
+
+  const workspaces = await getWorkspaces();
+  if (workspaces.total === 0) {
+    redirect("/workspaces/create");
+  } else {
+    redirect(`/workspaces/${workspaces.documents[0].$id}`);
+  }
   return (
     <div>
-      <CreateWorkspaceForm />
+      Home page
+      {/* <CreateWorkspaceForm /> */}
     </div>
   );
 }
