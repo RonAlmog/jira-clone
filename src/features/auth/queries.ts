0aa63ee308842f37f71766471143a83d97f0b123
Ron Almog <ron8132@gmail.com>
2024-11-18 16:31:04 -0500

    improve getCurrent with createSessionClient

--- src/features/auth/queries.ts
+++ src/features/auth/queries.ts
@@ -1,23 +1,12 @@
 "use server";
-
-import { cookies } from "next/headers";
-import { Account, Client } from "node-appwrite";
-import { AUTH_COOKIE } from "./constants";
+import { createSessionClient } from "@/lib/appwrite";
 
 export const getCurrent = async () => {
   try {
-    const client = new Client()
-      .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-      .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
+    const { account } = await createSessionClient();
 
-    // need to await cookies in next 15
-    const session = await cookies().get(AUTH_COOKIE);
-    if (!session) return null;
-
-    client.setSession(session.value);
-    const account = new Account(client);
     return await account.get();
-  } catch (error) {
+  } catch {
     return null;
   }
 };
