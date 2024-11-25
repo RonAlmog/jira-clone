f2dbd0463ed42daac6a30e3c8a0a6e2c5ee9aff3
Ron Almog <ron8132@gmail.com>
2024-11-25 13:48:31 -0500

    add project avatar

---
+++ src/features/projects/components/project-avatar.tsx
@@ -0,0 +1,41 @@
+import { cn } from "@/lib/utils";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import Image from "next/image";
+
+interface ProjectAvatarProps {
+  image?: string;
+  name: string;
+  className?: string;
+  fallbackClassName?: string;
+}
+
+export const ProjectAvatar = ({
+  image,
+  name,
+  className,
+  fallbackClassName,
+}: ProjectAvatarProps) => {
+  // avatar inside select is causing flicker, so we use simple image here
+  if (image) {
+    return (
+      <div
+        className={cn("size-5 relative rounded-md overflow-hidden", className)}
+      >
+        <Image src={image} alt={name} fill className="object-contain" />
+      </div>
+    );
+  }
+
+  return (
+    <Avatar className={cn("size-5 rounded-md", className)}>
+      <AvatarFallback
+        className={cn(
+          "text-white bg-blue-600 font-semibold text-sm uppercase rounded-md",
+          fallbackClassName
+        )}
+      >
+        {name[0]}
+      </AvatarFallback>
+    </Avatar>
+  );
+};
