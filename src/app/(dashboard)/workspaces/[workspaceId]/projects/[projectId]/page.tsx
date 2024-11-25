6cdb4021343e154be7028f9356bc96e00b523454
Ron Almog <ron8132@gmail.com>
2024-11-25 16:47:24 -0500

    handle errors on project page

--- src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
@@ -1,4 +1,9 @@
+import { Button } from "@/components/ui/button";
 import { getCurrent } from "@/features/auth/queries";
+import { ProjectAvatar } from "@/features/projects/components/project-avatar";
+import { getProject } from "@/features/projects/queries";
+import { PencilIcon } from "lucide-react";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
 interface ProjectPageProps {
@@ -8,7 +13,33 @@ const ProjectPage = async ({ params }: ProjectPageProps) => {
   const user = await getCurrent();
   if (!user) redirect("/sign-in");
 
-  return <div>projectId: {params.projectId}</div>;
+  const initialValues = await getProject({ projectId: params.projectId });
+  if (!initialValues) {
+    throw new Error("Project not found");
+  }
+
+  return (
+    <div className="flex flex-col gap-y-4">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-x-2">
+          <ProjectAvatar
+            name={initialValues.name}
+            image={initialValues.imageUrl}
+            className="size-8"
+          />
+          <p className="text-lg font-semibold">{initialValues.name}</p>
+        </div>
+        <Button variant="secondary" size="sm" asChild>
+          <Link
+            href={`/workspaces/${initialValues.workspaceId}/projects/${initialValues.$id}`}
+          >
+            <PencilIcon className="size-4 mr-2" />
+            Edit Project
+          </Link>
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectPage;
--- src/features/projects/queries.ts
+++ src/features/projects/queries.ts
@@ -34,50 +34,23 @@ interface GetProjectProps {
   projectId: string;
 }
 export const getProject = async ({ projectId }: GetProjectProps) => {
-  try {
-    const { databases, account } = await createSessionClient();
-    const user = await account.get();
-    // load the project
-    const project = await databases.getDocument<Project>(
-      DATABASE_ID,
-      PROJECTS_ID,
-      projectId
-    );
-    // find out if the member belong to the workspace of that project.
-    const member = await getMember({
-      databases,
-      userId: user.$id,
-      workspaceId: project.workspaceId,
-    });
-    if (!member) {
-      return null;
-    }
-
-    return project;
-  } catch {
-    return null;
+  const { databases, account } = await createSessionClient();
+  const user = await account.get();
+  // load the project
+  const project = await databases.getDocument<Project>(
+    DATABASE_ID,
+    PROJECTS_ID,
+    projectId
+  );
+  // find out if the member belong to the workspace of that project.
+  const member = await getMember({
+    databases,
+    userId: user.$id,
+    workspaceId: project.workspaceId,
+  });
+  if (!member) {
+    throw new Error("Unauthorized");
   }
-};
 
-// this allows get workspace without being a member yet.
-// for the join thru link
-export const getWorkspaceInfo = async ({ workspaceId }: GetWorkspaceProps) => {
-  try {
-    const { databases } = await createSessionClient();
-
-    // no need to be a member
-
-    const workspace = await databases.getDocument<Workspace>(
-      DATABASE_ID,
-      WORKSPACES_ID,
-      workspaceId
-    );
-
-    // for security, only return name
-    return {
-      name: workspace.name,
-    };
-  } catch {
-    return null;
-  }
+  return project;
 };
