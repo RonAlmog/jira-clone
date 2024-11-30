5a4de940f321304e101265d95a6f6a8da79ba51e
Ron Almog <ron8132@gmail.com>
2024-11-30 09:06:46 -0500

    add tasks table

---
+++ src/features/tasks/components/columns.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { ColumnDef } from "@tanstack/react-table";
+import { Task } from "../types";
+
+export const columns: ColumnDef<Task>[] = [
+  {
+    accessorKey: "name",
+    header: "Task Name",
+  },
+];
