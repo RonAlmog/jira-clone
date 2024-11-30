19924dfff80aa18028c897613537c09c24fbfd0a
Ron Almog <ron8132@gmail.com>
2024-11-30 10:33:20 -0500

    add pagination, sorting, filtering

--- src/features/tasks/components/columns.tsx
+++ src/features/tasks/components/columns.tsx
@@ -1,10 +1,23 @@
 "use client";
 import { ColumnDef } from "@tanstack/react-table";
 import { Task } from "../types";
+import { Button } from "@/components/ui/button";
+import { ArrowUpDown } from "lucide-react";
 
 export const columns: ColumnDef<Task>[] = [
   {
     accessorKey: "name",
-    header: "Task Name",
+
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Task Name
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
   },
 ];
