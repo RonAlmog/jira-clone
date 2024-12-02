8f61496b3c9aec71b1776d7d9a46253ce93a75f5
Ron Almog <ron8132@gmail.com>
2024-12-01 23:13:10 -0500

    add more actions to task menu

--- src/features/tasks/components/task-actions.tsx
+++ src/features/tasks/components/task-actions.tsx
@@ -4,7 +4,7 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { ExternalLinkIcon } from "lucide-react";
+import { ExternalLinkIcon, PencilIcon, TrashIcon } from "lucide-react";
 import React from "react";
 
 interface TaskActionProps {
@@ -26,6 +26,31 @@ export const TaskAction = ({ id, projectId, children }: TaskActionProps) => {
           >
             <ExternalLinkIcon className="size-4 mr-2 stroke-2" />
             Task Details
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            onClick={() => {}}
+            disabled={false}
+            className="font-medium p-[10px]"
+          >
+            <ExternalLinkIcon className="size-4 mr-2 stroke-2" />
+            Open Project
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            onClick={() => {}}
+            disabled={false}
+            className="font-medium p-[10px]"
+          >
+            <PencilIcon className="size-4 mr-2 stroke-2" />
+            Edit Task
+          </DropdownMenuItem>
+
+          <DropdownMenuItem
+            onClick={() => {}}
+            disabled={false}
+            className="text-amber-700 focus:text-amber-700 font-medium p-[10px]"
+          >
+            <TrashIcon className="size-4 mr-2 stroke-2" />
+            Delete Task
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
