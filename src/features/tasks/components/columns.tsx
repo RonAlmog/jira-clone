f253bed121f73b4e42ca29dd56d3af5bf3e367cc
Ron Almog <ron8132@gmail.com>
2024-12-01 22:41:48 -0500

    add options menu to task list

--- src/features/tasks/components/columns.tsx
+++ src/features/tasks/components/columns.tsx
@@ -2,12 +2,13 @@
 import { ColumnDef } from "@tanstack/react-table";
 import { Task } from "../types";
 import { Button } from "@/components/ui/button";
-import { ArrowUpDown } from "lucide-react";
+import { ArrowUpDown, MoreVertical } from "lucide-react";
 import { ProjectAvatar } from "@/features/projects/components/project-avatar";
 import { MemberAvatar } from "@/features/members/components/member-avatar";
 import TaskDate from "./task-date";
 import { Badge } from "@/components/ui/badge";
 import { snakeCaseToTitleCase } from "@/lib/utils";
+import { TaskAction } from "./task-actions";
 
 export const columns: ColumnDef<Task>[] = [
   {
@@ -117,6 +118,20 @@ export const columns: ColumnDef<Task>[] = [
     cell: ({ row }) => {
       const status = row.original.status;
       return <Badge variant={status}>{snakeCaseToTitleCase(status)}</Badge>;
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const id = row.original.$id;
+      const projectId = row.original.projectId;
+      return (
+        <TaskAction id={id} projectId={projectId}>
+          <Button variant="ghost" className="size-8 p-0">
+            <MoreVertical className="size-4" />
+          </Button>
+        </TaskAction>
+      );
     },
   },
 ];
