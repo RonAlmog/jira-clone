d7a838c7458c1dfea80ea1b89d2f042deac550af
Ron Almog <ron8132@gmail.com>
2024-12-07 10:02:46 -0500

    add task overview

--- src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { DottedSeparator } from "@/components/dotted-separator";
 import PageError from "@/components/page-error";
 import PageLoader from "@/components/page-loader";
 import { useGetTask } from "@/features/tasks/api/use-get-task";
 import { TaskBreadcrumbs } from "@/features/tasks/components/task-breadcrumbs";
+import TaskOverview from "@/features/tasks/components/task-overview";
 
 import { useTaskId } from "@/features/tasks/hooks/use-task-id";
 
@@ -20,6 +22,10 @@ export const TaskPageClient = () => {
   return (
     <div className="flex flex-col">
       <TaskBreadcrumbs project={data?.project!} task={data!} />
+      <DottedSeparator className="my-6" />
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+        <TaskOverview task={data!} />
+      </div>
     </div>
   );
 };
