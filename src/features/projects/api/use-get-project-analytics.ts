5f655a1c7643e224af5889702628c25babf54080
Ron Almog <ron8132@gmail.com>
2024-12-10 21:30:04 -0500

    add analytics to project page

---
+++ src/features/projects/api/use-get-project-analytics.ts
@@ -0,0 +1,36 @@
+import { useQuery } from "@tanstack/react-query";
+import { client } from "@/lib/rpc";
+import { InferResponseType } from "hono";
+
+interface UseGetPorjectAnlayticsProps {
+  projectId: string;
+}
+
+export type ProjectAnalyticsResponse = InferResponseType<
+  (typeof client.api.projects)[":projectId"]["analytics"]["$get"],
+  200
+>;
+
+export const useGetProjectAnalytics = ({
+  projectId,
+}: UseGetPorjectAnlayticsProps) => {
+  const query = useQuery({
+    queryKey: ["project-analytics", projectId],
+    queryFn: async () => {
+      const response = await client.api.projects[":projectId"][
+        "analytics"
+      ].$get({
+        param: { projectId },
+      });
+      if (!response.ok) {
+        // return null;
+        throw new Error("Failed to fetch project analytics");
+      }
+
+      const { data } = await response.json();
+      return data;
+    },
+  });
+
+  return query;
+};
