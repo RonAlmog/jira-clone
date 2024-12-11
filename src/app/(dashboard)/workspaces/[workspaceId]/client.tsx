18b2492352dfe76954675fa30c7895f885d12b9e
Ron Almog <ron8132@gmail.com>
2024-12-11 13:19:28 -0500

    add project-list, task-list

--- src/app/(dashboard)/workspaces/[workspaceId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/client.tsx
@@ -1,28 +1,19 @@
 "use client";
 
 import Analytics from "@/components/analytics";
-import { DottedSeparator } from "@/components/dotted-separator";
+
 import PageError from "@/components/page-error";
 import PageLoader from "@/components/page-loader";
-import { Button } from "@/components/ui/button";
+
 import { useGetMembers } from "@/features/members/api/use-get-members";
 import { useGetProjects } from "@/features/projects/api/use-get-projects";
 import { useCreateProjectModal } from "@/features/projects/hooks/use-create-project-modal";
 import { useGetTasks } from "@/features/tasks/api/use-get-tasks";
-import { useCreateTaskModal } from "@/features/tasks/hooks/use-create-task-modal";
-import { Task } from "@/features/tasks/types";
+
 import { useGetWorkspaceAnalytics } from "@/features/workspaces/api/use-get-workspace-analytics";
 import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
-import { PlusIcon, CalendarIcon, SettingsIcon } from "lucide-react";
-import {
-  Card,
-  CardContent,
-  CardTitle,
-  CardHeader,
-  CardDescription,
-} from "@/components/ui/card";
-import { formatDistanceToNow } from "date-fns";
-import Link from "next/link";
+import { TaskList } from "./task-list";
+import { ProjectList } from "./project-list";
 
 const WorkspaceIdClient = () => {
   const workspaceId = useWorkspaceId();
@@ -58,55 +49,10 @@ const WorkspaceIdClient = () => {
       <Analytics data={analytics} />
       <div className="grid grid-cols-1 xl:grid-cols-2 gap-4">
         <TaskList tasks={tasks.documents} total={tasks.total} />
+        <ProjectList projects={projects.documents} total={projects.total} />
       </div>
     </div>
   );
 };
 
 export default WorkspaceIdClient;
-
-interface TaskListProps {
-  tasks: Task[];
-  total: number;
-}
-
-export const TaskList = ({ tasks, total }: TaskListProps) => {
-  const workspaceId = useWorkspaceId();
-  const { open: createTask } = useCreateTaskModal();
-  return (
-    <div className="flex flex-col gap-y-4 col-span-1">
-      <div className="bg-muted rounded-lg p-4">
-        <div className="flex items-center justify-between">
-          <p>Tasks ({total})</p>
-          <Button variant="muted" size="icon" onClick={createTask}>
-            <PlusIcon className="size-4 text-neutral-400" />
-          </Button>
-        </div>
-        <DottedSeparator className="my-4" />
-        <ul className="flex flex-col gap-y-4">
-          {tasks.map((task) => (
-            <li key={task.$id}>
-              <Link href={`/workspaces/${workspaceId}/tasks/${task.$id}`}>
-                <Card className="shadow-none rounded-lg hover:opacity-75 transition">
-                  <CardContent className="p-4">
-                    <p className="text-lg font-medium truncate">{task.name}</p>
-                    <div className="flex items-center gap-x-2">
-                      <p>{task.project?.name}</p>
-                      <div className="size-1 rounded-full bg-neutral-200" />
-                      <div className="text-sm text-muted-foreground flex items-center">
-                        <CalendarIcon className="size-3 mr-1" />
-                        <span className="truncate">
-                          {formatDistanceToNow(new Date(task.dueDate))}
-                        </span>
-                      </div>
-                    </div>
-                  </CardContent>
-                </Card>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
-};
