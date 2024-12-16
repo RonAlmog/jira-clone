28f7d0344a792e993672dac54200b36b4a382861
Ron Almog <ron8132@gmail.com>
2024-12-16 09:25:37 -0500

    complete all collections

--- src/lib/collections.ts
+++ src/lib/collections.ts
@@ -11,4 +11,19 @@ export const collections: DbType[] = [
     id: process.env.NEXT_PUBLIC_APPWRITE_PROJECTS_ID!,
     name: "projects",
   },
+  {
+    databaseId: process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID!,
+    id: process.env.NEXT_PUBLIC_APPWRITE_MEMBERS_ID!,
+    name: "members",
+  },
+  {
+    databaseId: process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID!,
+    id: process.env.NEXT_PUBLIC_APPWRITE_MEMBERS_ID!,
+    name: "members",
+  },
+  {
+    databaseId: process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID!,
+    id: process.env.NEXT_PUBLIC_APPWRITE_TASKS_ID!,
+    name: "tasks",
+  },
 ];
--- src/lib/get-db.ts
+++ src/lib/get-db.ts
@@ -39,7 +39,5 @@ export const getDb = (databases: Databases) => {
     };
   });
 
-  console.log({ db });
-
   return db;
 };
