85bc626d44ad1c0468699dc0296a0ebaaf9eff7c
Ron Almog <ron8132@gmail.com>
2024-12-16 20:16:29 -0500

    add ws dummy data

---
+++ src/features/seeding/data/workspaces.ts
@@ -0,0 +1,17 @@
+export type SeedWorkspace = {
+  name: string;
+  imageUrl?: string;
+  userId?: string;
+};
+
+export const workspaces: SeedWorkspace[] = [
+  {
+    name: "MiCasa Mortgage Application",
+  },
+  {
+    name: "MyCard Banking Application",
+  },
+  {
+    name: "Back office applications",
+  },
+];
