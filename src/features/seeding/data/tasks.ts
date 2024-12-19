0938025d3b5d4e7d38a3558d957509867d17e0f0
Ron Almog <ron8132@gmail.com>
2024-12-19 10:42:38 -0500

    add dummy task list for seeding

--- src/features/seeding/data/tasks.ts
+++ src/features/seeding/data/tasks.ts
@@ -4,18 +4,79 @@ export type SeedTask = {
 
 export const tasks: SeedTask[] = [
   {
-    description: "Backend with AppWrite",
+    description: "Add login page with validation",
   },
   {
-    description: "Main website, Next.js",
+    description: "Add registration page",
   },
   {
-    description: "DevOps",
+    description: "Add user auth with JWT",
   },
   {
-    description: "E2E Testing",
+    description: "Fix bug 1341",
   },
   {
-    description: "QA",
+    description: "Fix bug 1401",
+  },
+  {
+    description: "Refactor backend route for shopping cart",
+  },
+  {
+    description: "Refactor order confirmation backend route",
+  },
+  {
+    description: "Add 2 factor authentication",
+  },
+  {
+    description: "Allow users to upload product images",
+  },
+  {
+    description: "Add picture gallery to product page",
+  },
+  {
+    description: "Add content to home page",
+  },
+  {
+    description: "Add about us page with content from CMS",
+  },
+  {
+    description: "Add terms page with content from CMS",
+  },
+  {
+    description: "Add consent checkbox to sign-up. link to terms page",
+  },
+  {
+    description:
+      "Make product page responsive. Support mobile, tablet, desktop",
+  },
+  {
+    description: "Fix bug on sidebar menu",
+  },
+  {
+    description: "Add Analytics to category pages",
+  },
+  {
+    description: "Add react query hooks for api endpoints",
+  },
+  {
+    description: "Add playwright test to shopping cart page",
+  },
+  {
+    description: "Build CI/CD pipeline with github actions",
+  },
+  {
+    description: "Create state modal pages with Zustand",
+  },
+  {
+    description: "Implement remove item from cart",
+  },
+  {
+    description: "Cleanup all build errors",
+  },
+  {
+    description: "Add useConfirm modal",
+  },
+  {
+    description: "Add a loading page",
   },
 ];
